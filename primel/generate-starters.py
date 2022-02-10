ca77d11e4be779dbba8cc88e7e6b42dbf48efc29
Emmanuella Rumanti <amuritna@gmail.com>
2022-02-10 20:53:45 +0700

    Update generate-starters.py

--- primel/generate-starters.py
+++ primel/generate-starters.py
@@ -3,27 +3,24 @@ import sympy
 """
 Generates a prime number with five unique digits.
 @wantedDigits Only print if it features digits included in this set. Digits must be as strings. Empty set by default.
+@unwantedDigits Only print if it DOES NOT feature digits included in this set. Digits must be as strings. Empty set by default.
 
-Example usages: generateStarter(), generateStarter({'1'}), generateStarter({'8', '7', '0,'}), generateStarter(set("123"))
+Example usages:
+generateStarter()
+generateStarter({'1'})
+generateStarter({'8', '7', '0,'})
+generateStarter(set("123"))
+generateStarter(set("239"), set("17"))
+
 """
-def generateStarter(wantedDigits={}):
 
-    # no specific wanted digits
-    if len(wantedDigits) == 0:
+def generateStarter(wantedDigits=set({}), unwantedDigits=set({})):
 
-        # brute-force check all 5-digit primes (not very efficient as of now)
-        for i in sympy.primerange(10000, 100000):
+    # brute-force check all 5-digit primes (not very efficient as of now)
+    for i in sympy.primerange(10000, 100000):
 
-            # print if all digits are unique
-            if len(set(str(i))) == 5:
-                print(i, end=", ")
-    
-    else:
+        gen = set(str(i))
 
-        # same case as above
-        for i in sympy.primerange(10000, 100000):
-
-            gen = set(str(i))
-
-            if len(gen) == 5 and wantedDigits.issubset(gen):
-                print(i, end=", ")
+        # print if all digits are unique
+        if len(gen) == 5 and wantedDigits.issubset(gen) and unwantedDigits.isdisjoint(gen):
+            print(i, end=", ")
